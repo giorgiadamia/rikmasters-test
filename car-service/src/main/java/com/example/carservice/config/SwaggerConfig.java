da739eaa79e0a28348b74d740d23e5f1f4f9eb32
Giorgi <1giorgiadamia@gmail.com>
2023-06-30 13:02:59 +0400

    added swagger

---
+++ car-service/src/main/java/com/example/carservice/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.carservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Rickmasters test task")
+                        .description("Car Service")
+                        .version("1.0")
+                );
+    }
+}
